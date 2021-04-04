61f6586ab8cf36d27920cd9ca3a96e9e9fc55aa0
mariam ashraf <mariam.ashraf@safavisa.com>
2021-04-04 15:10:51 +0200

    First Version

---
+++ MobileTestAutomationFramework/src/test/java/com/todoqacart/testcases/LoginScreenTest.java
@@ -0,0 +1,27 @@
+package com.todoqacart.testcases;
+
+import org.testng.annotations.Test;
+
+public class LoginScreenTest {
+    @Test
+    public void login_Test_With_Valid_Credintials(){
+
+    }
+
+    @Test
+    public void login_Test_With_Empty_Email(){
+
+    }
+
+    @Test
+    public void login_Test_With_Empty_Password(){
+
+    }
+
+    @Test
+    public void login_Test_Click_On_Signup(){
+
+    }
+
+
+}
---
+++ MobileTestAutomationFramework/src/test/java/com/todoqacart/testcases/SignUpScreenTest.java
@@ -0,0 +1,21 @@
+package com.todoqacart.testcases;
+
+import org.testng.annotations.Test;
+
+public class SignUpScreenTest {
+    @Test
+    public void signup_With_Valid_Information(){
+
+    }
+
+    @Test
+    public void click_On_Login(){
+
+    }
+
+    @Test
+    public void signup_Without_Email(){
+
+    }
+
+}
